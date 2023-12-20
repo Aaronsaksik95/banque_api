11cfc592e0602f1ef15ae1d069650584521513fb
Aaronsaksik95 <48072077+Aaronsaksik95@users.noreply.github.com>
2023-12-20 21:10:50 +0100

    routes and controller accounts

--- src/controllers/accounts.controller.js
+++ src/controllers/accounts.controller.js
@@ -1,40 +1,163 @@
 import Account from '../models/account.model.js';
+import Operation from '../models/operation.model.js';
+import User from '../models/user.model.js';
 
-export const getAccountId = (req, res) => {
+export const getAccountId = async (req, res) => {
+    const accountId = req.params.id;
 
-}
-
-export const getAllAccount = (req, res) => {
-    Account.find()
-        .then((data) => {
-            if (data != null) {
-                res.status(200).json({
-                    accounts: data,
-                    response: true
-                });
-            } else {
-                res.status(404).json({
-                    accounts: null,
-                    response: false
-                });
-            }
-        })
-        .catch((err) => {
-            res.status(500).json({
-                error: 500,
-                message: err.message || 'Server Error'
-            });
+    try {
+        const account = await Account.findById(accountId);
+        res.status(200).json({
+            account: account,
+            response: true
         });
-}
+    } catch (err) {
+        res.status(500).json({
+            error: 500,
+            message: err.message || 'Server Error'
+        });
+    }
+};
 
-export const createAccount = (req, res) => {
 
-}
+export const getAllAccount = async (req, res) => {
+    const userId = req.user.id;
 
-export const updateAccount = (req, res) => {
+    try {
+        const accounts = await Account.find({ user: userId });
+        res.status(200).json({
+            accounts: accounts,
+            response: true
+        });
+    } catch (err) {
+        res.status(500).json({
+            error: 500,
+            message: err.message || 'Server Error'
+        });
+    }
+};
 
-}
 
-export const deleteAccount = (req, res) => {
+export const createAccount = async (req, res) => {
+    const userId = req.user.id;
 
-}
+    try {
+        const newAccount = new Account({
+            ...req.body,
+            user: userId
+        });
+        const account = await newAccount.save();
+
+        await User.findByIdAndUpdate(userId, { $push: { accounts: account._id } }, { new: true })
+        res.status(201).json({
+            account: account,
+            response: true,
+        });
+    } catch (err) {
+        res.status(400).json({
+            error: 400,
+            message: err.message || 'Bad Request',
+        });
+    }
+};
+
+export const depositIntoAccount = async (req, res) => {
+    const { amount } = req.body;
+    const sendingAccountId = req.params.id;
+    const userId = req.user.id;
+
+    try {
+        const account = await Account.findByIdAndUpdate(sendingAccountId, { $inc: { amount: amount } }, { new: true });
+        await createOperation('deposit', 'accepted', amount, userId, sendingAccountId);
+        res.status(200).json({
+            deposit: true,
+            account: account,
+        });
+
+    } catch (err) {
+        res.status(400).json({
+            error: 400,
+            message: err.message || 'Bad Request',
+        });
+    }
+};
+
+export const withdrawalIntoAccount = async (req, res) => {
+    const { amount } = req.body;
+    const sendingAccountId = req.params.id;
+    const userId = req.user.id;
+
+    try {
+        const account = await Account.findById(sendingAccountId);
+        if (!account || account.overdraft + account.amount < amount) {
+            await createOperation('withdrawal', 'denied', amount, userId, sendingAccountId);
+            return res.status(200).json({
+                withdrawal: false,
+                message: 'Withdrawal denied, below your authorized overdraft.',
+                account: account
+            });
+        }
+
+        const updatedAccount = await Account.findByIdAndUpdate(sendingAccountId, { $inc: { amount: -amount } }, { new: true });
+        await createOperation('withdrawal', 'accepted', amount, userId, sendingAccountId);
+        return res.status(200).json({
+            withdrawal: true,
+            account: updatedAccount
+        });
+
+    } catch (err) {
+        res.status(400).json({
+            error: 400,
+            message: err.message || 'Bad Request',
+        });
+    }
+};
+
+export const transferAccount = async (req, res) => {
+    const sendingAccountId = req.body.sendingAccount
+    const receivingAccountId = req.body.receivingAccount
+    const amount = req.body.amount
+    const userId = req.user.id;
+
+    try {
+        const sendingAccount = await Account.findById(sendingAccountId);
+        const receivingAccount = await Account.findById(receivingAccountId);
+        if (!sendingAccount || sendingAccount.overdraft + sendingAccount.amount < amount) {
+            await createOperation('transfer', 'denied', amount, userId, sendingAccountId, receivingAccountId);
+            return res.status(200).json({
+                transfer: false,
+                message: 'Transfer denied, below your authorized overdraft.',
+                sendingAccount: sendingAccount,
+                receivingAccount: receivingAccount
+            });
+        }
+
+        const updatedSendingAccount = await Account.findByIdAndUpdate(sendingAccountId, { $inc: { amount: -amount } }, { new: true });
+        const updatedReceivingAccount = await Account.findByIdAndUpdate(receivingAccountId, { $inc: { amount: amount } }, { new: true });
+        await createOperation('transfer', 'accepted', amount, userId, sendingAccountId, receivingAccountId);
+        return res.status(200).json({
+            transfer: true,
+            SendingAccount: updatedSendingAccount,
+            ReceivingAccount: updatedReceivingAccount
+        });
+
+    } catch (err) {
+        res.status(400).json({
+            error: 400,
+            message: err.message || 'Bad Request',
+        });
+    }
+};
+
+const createOperation = async (type, status, amount, userId, sendingAccountId, receivingAccountId) => {
+    const newOperation = new Operation({
+        type: type,
+        status: status,
+        amount: amount,
+        user: userId,
+        sendingAccount: sendingAccountId,
+        receivingAccount: receivingAccountId
+    });
+    return newOperation.save();
+};
+
--- src/routes/accounts.route.js
+++ src/routes/accounts.route.js
@@ -1,13 +1,22 @@
 import { Router } from 'express';
 import verifyToken from '../middlewares/verifyToken.js'
-import { getAllAccount, getAccountId, createAccount, updateAccount, deleteAccount } from '../controllers/accounts.controller.js';
+import {
+    getAllAccount,
+    getAccountId,
+    createAccount,
+    depositIntoAccount,
+    withdrawalIntoAccount,
+    transferAccount
+} from '../controllers/accounts.controller.js';
+
 const router = Router();
 
 router.get('/', verifyToken, getAllAccount);
 router.get('/:id', verifyToken, getAccountId);
 router.post('/', verifyToken, createAccount);
-router.put('/:id', verifyToken, updateAccount);
-router.delete('/:id', verifyToken, deleteAccount);
+router.patch('/:id/deposit', verifyToken, depositIntoAccount);
+router.patch('/:id/withdraw', verifyToken, withdrawalIntoAccount);
+router.post('/transfer', verifyToken, transferAccount);
 
 
 export default router;
